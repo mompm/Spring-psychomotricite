2749182411e945d4a2e1bf12657ee88f992f2dd1
mompm <monabudeau@gmail.com>
2025-01-23 23:40:12 +0100

    initial commit ( front+back)

---
+++ backend/src/main/java/com/psychomot/backend/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.psychomot.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorldController {
+    
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
